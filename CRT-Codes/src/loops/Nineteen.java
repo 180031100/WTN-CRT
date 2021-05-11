47e863eae44c8e8add05bd4a66e24f7f6990697d
180031100 <180031100@kluniversity.in>
2021-05-11 12:36:22 +0530

    Practice on Loops and Arrays

---
+++ CRT-Codes/src/loops/Nineteen.java
@@ -0,0 +1,20 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Nineteen {
+  public static void main(String[] args) 
+  {
+    // TODO Auto-generated method stub
+    int i=1,c=5;
+    while(c>0)
+    {
+      if(i%2==0 && i%3==0 && i%5==0)
+      {
+        System.out.println(i+" ");
+        c--;
+      }
+      i++;
+    }
+  }
+}
